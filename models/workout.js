dd6c256a4df6053530051608a5737dcfad1e6a4d
Malcolm Mason <79657911+malmason@users.noreply.github.com>
2021-07-23 15:35:08 -0400

    Created model and seeded database

---
+++ models/workout.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date, 
+    deafulat: Date.now
+  },
+  exercises: [
+    {
+      type: String,
+      name: String,
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
+
+
+
+
